5f2e6186851439b47fa1d2946291243d803c1734
HERO-KUN <herokun.user@gmail.com>
2020-09-10 09:35:14 +0900

    Creation of ShootingStar.kt with default configurations.

---
+++ doodle/src/main/java/com/herok/doodle/ShootingStar.kt
@@ -0,0 +1,19 @@
+package com.herok.doodle
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class ShootingStar(context: Context, attrs: AttributeSet?): FrameLayout(context, attrs) {
+
+    constructor(context: Context): this(context, null)
+
+    init {
+
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+}
